3b6d0163b2379d4a46453f6dbfa7953756ef452e
Sergey Medvedev <Sergey.Medvedev@cadfem-cis.ru>
2019-09-17 16:52:10 +0300

    Decreased query count to database

--- service/views.py
+++ service/views.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Service, Location
+from .models import Location, APIKeyStore
 
 
 class WeatherAPIView(APIView):
@@ -12,11 +12,11 @@ class WeatherAPIView(APIView):
     def get(self, request, *args, **kwargs):
         try:
             location = Location.objects.get(default=True)
-            service = Service.objects.get(default=True)
-            api_key = service.apikeystore_set.get(current_key=True)
+            api_key_obj = APIKeyStore.objects.select_related('service').get(current_key=True, service__default=True)
+            service = api_key_obj.service
         except ObjectDoesNotExist as e:
             return Response({'error': str(e)})
-        payload = {'appid': api_key.key, 'units': 'metric',
+        payload = {'appid': api_key_obj.key, 'units': 'metric',
                    'lat': location.lat, 'lon': location.lon}
         r = requests.get(service.url, params=payload)
 
